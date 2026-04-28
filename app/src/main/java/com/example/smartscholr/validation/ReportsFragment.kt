a97de603a7886e70f009e9bd4cfe465a6f7838d1
zwanefistos <zwanefistos88@gmail.com>
2026-04-28 18:45:34 +0200

    Setting up the js interop chart for apache echarts

---
+++ app/src/main/java/com/example/smartscholr/validation/ReportsFragment.kt
@@ -0,0 +1,12 @@
+package com.example.smartscholr.validation
+
+import android.webkit.WebView
+import com.example.smartscholr.R
+
+val webView: WebView = findViewById(R.id.reportingChart)
+
+fun findViewById(reportingChart: Any): WebView {
+    TODO("Not yet implemented")
+}
+
+
