338f025420d2245835f0f5e0985362519f8446fc
Nduduzo Jali <ST10402988@rcconnect.edu.za>
2026-04-28 19:57:22 +0200

    Add dashboard fragment with UI

---
+++ app/src/main/java/com/example/smartscholr/dashboard/DashboardFragment.kt
@@ -0,0 +1,18 @@
+package com.example.smartscholr.dashboard
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.smartscholr.R
+
+class DashboardFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_dashboard, container, false)
+    }
+}
